d94f4c769d9f1eda4c79dad6d8760444e062196e
pajarraco <mail@pajarraco.com>
2021-05-28 09:14:40 +1000

    Clean code

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,8 +5,8 @@ function HomePage() {
   return (
     <div className='container'>
       <h1>Home</h1>
-      <p>This is your awesome HomePage 
-      <br> <Link to='/ernesto'>User Ernesto</Link></p>
+      <p>This is your awesome HomePage
+      <br /><Link to='/ernesto'>User Ernesto</Link></p>
     </div>
   );
 }
