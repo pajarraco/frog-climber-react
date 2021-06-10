b22b894350758340eb150059fafca6abdad47c8a
pajarraco <mail@pajarraco.com>
2021-06-11 09:32:19 +1000

    add more test - mock hooks useParams

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import './app.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
--- src/header/header.test.js
+++ src/header/header.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Header from './Header';
+import Header from './header';
 
 describe('Header', () => {
   test('should render header', () => {
