64442fff5ebb598d2f3d434a69bbedf7637aab06
pajarraco <mail@pajarraco.com>
2021-06-17 14:16:26 +1000

    add build folder for git pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './app.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+library.add(fab, fas);
 
 import Header from './header/header';
 import Footer from './footer/footer';
@@ -13,8 +14,6 @@ import Footer from './footer/footer';
 import HomePage from './pages/home/home_page';
 import UserPage from './pages/user/user_page';
 import FollowPage from './pages/follow/follow_page';
-
-library.add(fab, fas);
 
 function App() {
   return (
--- src/pages/follow/follow_page.test.js
+++ src/pages/follow/follow_page.test.js
@@ -1,10 +1,23 @@
 import { render, screen } from '@testing-library/react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fab, fas);
+
 import FollowPage from './follow_page';
 
 describe('Follow Page', () => {
   test('should render follow page', () => {
     render(<FollowPage />);
-
     expect(screen.getByText(/Follow Us/)).toBeInTheDocument();
   });
+
+  test('should render 5 links', () => {
+    render(<FollowPage/>);
+    const links = screen.getAllByRole('link');
+
+    expect(links.length).toBe(5);
+  });
+
 });
